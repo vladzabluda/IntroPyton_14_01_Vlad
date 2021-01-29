89a28a072a9e597ebc5998dde996fff498a97aa7
vladzabluda <vladzabluda@gmail.com>
2021-01-29 22:16:25 +0200

    #2

--- Homework #4.py
+++ Homework #4.py
@@ -1,4 +1,20 @@
+#1
+
+#my_int = "7020010060810340230200"
+#new_my_int = "0"
+#result = my_int.count(new_my_int)
+#print(result)
+
+#############################
+
+#2
+
 my_int = "7020010060810340230200"
-new_my_int = "0"
-result = my_int.count(new_my_int)
-print(result)
+count=0
+for e in str(my_int)[::-1]:
+    if e=='0':
+       count+=1
+    else:
+           break
+print(count)
+
