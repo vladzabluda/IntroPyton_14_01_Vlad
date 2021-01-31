03aea730ed4cab9da4869a5b2a6c9f33a8ff9fe6
vladzabluda <vladzabluda@gmail.com>
2021-01-31 13:51:03 +0200

    #2

--- Homework #4.py
+++ Homework #4.py
@@ -9,12 +9,23 @@
 
 #2
 
-my_int = "7020010060810340230200"
-count = 0
-for e in str(my_int)[::-1]:
-    if e =='0':
-       count += 1
-    else:
-           break
-print(count)
+#my_int = "7020010060810340230200"
+#count = 0
+#for e in str(my_int)[::-1]:
+#    if e =='0':
+#       count += 1
+#    else:
+#           break
+#print(count)
 
+#################################
+
+
+
+#####################################
+
+#3б
+
+#my_list_1, my_list_2 = [1, 3, 2, 4,5 ] , [10, 20, 15, 25, 22]
+#my_result = [my_list_1[i] for i in range(len(my_list_1)) if my_list_1[i] % 2 == 0] + [my_list_2[i] for i in range(len(my_list_2)) if my_list_2[i] % 2 == 1]
+#rint(my_result)
