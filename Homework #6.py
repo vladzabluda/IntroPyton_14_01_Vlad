ffc2f5e73dae034231b40e7bd36af96e87f173bd
vladzabluda <vladzabluda@gmail.com>
2021-02-02 16:39:26 +0200

    # 6

--- Homework #6.py
+++ Homework #6.py
@@ -51,11 +51,11 @@
 
 #6
 
-#my_str_1 = "Даны две строки"
+#my_str_1 = "Даны две строки "
 #my_str_2 = "Создать список"
 #my_set_1 = set(my_str_1)
 #my_set_2  = set(my_str_2)
-#result_set = my_set_1.union(my_set_2)
+#result_set = my_set_1.intersection(my_set_2)
 #print(result_set)
 
 ############################
