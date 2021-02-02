3e9bb4389067b97a114e4343a9560dff46cf2589
vladzabluda <vladzabluda@gmail.com>
2021-02-02 12:20:33 +0200

    #1

--- Homework #5.py
+++ Homework #5.py
@@ -33,7 +33,7 @@ print(my_result)
 
 my_list_1, my_list_2 = [1, 3, 2, 4,5 ] , [10, 20, 15, 25, 22]
 my_result = [my_list_1[i] for i in range(len(my_list_1)) if my_list_1[i] % 2 == 0] + [my_list_2[i] for i in range(len(my_list_2)) if my_list_2[i] % 2 == 1]
-rint(my_result)
+print(my_result)
 
 #######################################
 
--- Homework #6.py
+++ Homework #6.py
@@ -1,1 +1,14 @@
-1
+#1
+
+#my_list = ['дом', 'сын', 'дерево', 'жизнь']
+#new_my_list = []
+#for index, str in enumerate(my_list):
+#     if index % 2 == 0 :
+#         new_my_list.append(str[::-1])
+#     else:
+#         new_my_list.append(str)
+#print(new_my_list)
+
+#my_list = ['дом', 'сын', 'дерево', 'жизнь', 'автобус']
+#new_my_list = []
+
