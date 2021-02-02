24aa08cc375c66d8245f9f0e8b10ac8b1911da29
vladzabluda <vladzabluda@gmail.com>
2021-02-02 15:50:22 +0200

    # 5

--- Homework #6.py
+++ Homework #6.py
@@ -29,3 +29,24 @@
 
 ########################
 
+#4
+
+#my_list = ['дом', 'сын', 'дерево', 'жизнь', 'автобус', 11, 12, 13,14]
+#new_my_list = []
+#new_my_list = [value for value in my_list if type(value) == int]
+#print(new_my_list)
+
+#######################
+
+#5
+
+#my_str = "Дана строка"
+#new_my_str = ''
+#for i in range(len(my_str)):
+ #   if new_my_str.find(my_str[i]) == -1 and my_str[i] != ' ':
+ #       new_my_str += my_str[i]
+#print(new_my_str)
+
+
+#6
+
