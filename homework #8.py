3174e3fbc3c466e71c6a689962898123a64d09f2
vladzabluda <vladzabluda@gmail.com>
2021-02-12 17:21:47 +0200

    8

--- homework #8.py
+++ homework #8.py
@@ -7,17 +7,18 @@ import_names = random.choice(names)
 print(import_names)
 #
 i=0
-while i<3:
+while i<1:
     number = random.randint(100,999)
     print(number)
     i = i+1
+
 
 #
 import random
 import string
 def generate_random_string(length):
     letters = string.ascii_lowercase
-    rand_string = ''.join(random.choice(letters) for i in range(length))
+    rand_string = ''.join(random.choice(letters) for i in range(random.randint(5, 7)))
 
     print(rand_string)
     return rand_string
@@ -32,3 +33,46 @@ print(import_domains)
 
 print(str(import_names)+"."+str(number)+"@"+str(new_rand_string)+"."+str(import_domains))
 
+#
+
+import string
+import random
+
+
+def create_random_str():
+     rand_string = new_rand_string
+     count = random.randint(5,10)
+     return rand_string * count
+
+
+def create_spaces(new_rand_string):
+    index = 0
+    rand_str_to_list = list(new_rand_string)
+    condition = True
+    while condition:
+        step = random.randint(1, 10)
+        index += step
+        if index < len(new_rand_string):
+            rand_str_to_list[index] = " "
+        else:
+            condition = False
+    new_rand_string = "".join(rand_str_to_list)
+    return new_rand_string
+
+def modify_word(word):
+    return word.capitalize()
+
+def modify_str(new_rand_string):
+    new_rand_string_split = new_rand_string.split()
+    result = []
+    for word in new_rand_string_split:
+
+        word = modify_word(word)
+        result.append(word)
+    return " ".join(result)
+
+
+new_rand_string = create_random_str()
+new_rand_string = create_spaces(new_rand_string)
+new_rand_string = modify_str(new_rand_string)
+print(new_rand_string)
