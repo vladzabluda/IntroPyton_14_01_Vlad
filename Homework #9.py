21795a79748cc0a504a4642ffba07c579dd0ec46
vladzabluda <vladzabluda@gmail.com>
2021-02-17 14:23:54 +0200

    generate_and_write_json

--- Homework #9.py
+++ Homework #9.py
@@ -21,4 +21,41 @@ for i in data:
     print(lst[1])
 
 
+#################
 
+import json
+data = {"test_list": [data]}
+filename = "test_json_write.json"
+with open(filename, "w") as js_file:
+    json.dump(data, js_file)
+
+####################
+
+#import pprint
+import random
+import string
+
+
+def random_key():
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(5))
+
+
+def random_value():
+    return random.choice([
+        lambda: random.randint(-100, 100),
+        lambda: random.uniform(0, 1),
+        lambda: random.choice([False, True])
+    ])();
+
+def random_dict():
+    return {random_key(): random_value() for _ in range(random.randint(5, 20))}
+
+print(random_dict())
+
+####################
+
+import json
+data = {"generate_and_write_json": [random_dict()]}
+filename = "generate_and_write_json.json"
+with open(filename, "w") as js_file:
+    json.dump(data, js_file)
