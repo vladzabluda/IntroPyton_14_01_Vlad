ce9d9b5c9ca7b6427b06ed3f8b1c0f019afc4893
vladzabluda <vladzabluda@gmail.com>
2021-02-24 14:04:43 +0200

    10

--- Homework #10.py
+++ Homework #10.py
@@ -1,3 +1,4 @@
+
 import csv
 
 with open('read_file.csv') as File:
@@ -12,3 +13,30 @@ with open('write_file.csv', 'w', newline="") as csvfile:
     writer.writeheader()
     writer.writerow({'Age': 22, 'The country': 'Poland', 'Name': 'Ivan'})
     writer.writerow({'Age': 56, 'The country': 'USA', 'Name': 'Bob'})
+
+#####################
+path = 'C:\\Users\\admin\\PycharmProjects\\IntroPyton_14_01_Vlad\\read_file.txt'
+read_file = open(path,'r')
+read = read_file.read()
+print(read)
+
+with open('write_file.txt', 'w') as opened_file:
+    opened_file.write('qwertyu')
+    file = open('write_file', 'w')
+    try:
+        file.write('qwertyu!')
+    finally:
+        file.close()
+
+#################
+import json
+
+with open('read_file.json', 'r', encoding='utf-8') as f:
+    text = json.load(f)
+    print(text)
+
+import json
+data = {"write_file": [text]}
+filename = "write_file.json"
+with open(filename, "w") as js_file:
+    json.dump(data, js_file)
